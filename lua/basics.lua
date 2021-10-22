3dc053787428cb2e1f1a0309814e3ad7fc955818
Albin Groen <albin.groen@gmail.com>
2021-10-22 18:35:05 +0200

    :recycle: Replace all mappings and most basic settings with lua

--- lua/basics.lua
+++ lua/basics.lua
@@ -1,112 +1,42 @@
+vim.g.mapleader = " "
+vim.o.number  =true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.expandtab = true
+vim.o.incsearch = true
+vim.o.tabstop = 2
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.hlsearch = false
+vim.o.swapfile = false
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.scrolloff = 3
+vim.o.errorbells = false
+vim.o.shiftwidth = 2
+vim.o.numberwidth = 4
+vim.o.termguicolors = true
+vim.o.colorcolumn = '80'
+vim.o.showmode = false
+vim.o.showtabline = 2
+
+vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'tn', ':tabnew<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'tk', ':tabnext<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'tj', ':tabprev<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'to', ':tabo<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-N>', ':call ToggleNetrw()<CR>', { noremap = true })
+
 vim.api.nvim_exec(
 [[
-" Set leader to the spacebar
-let mapleader = "\<Space>"
-
 " Better code indentation
 filetype plugin indent on
-
-" Show line numbers
-set number
-
-" Do not wrap lines
-set nowrap
-
-" Convert tabs to spaces
-set expandtab
-
-" Show results while searching
-set incsearch
-
-" Tabs results in 2 spaces
-set tabstop=2
-
-" Highlight current line
-set cursorline
-
-" Ignore case when searching
-set ignorecase
-
-" Do not highlight search results
-set nohlsearch
-
-" Do not use swapfiles
-set noswapfile
-
-" Move cursor on horizontal split
-set splitbelow
-
-" Move cursor on vertical split
-set splitright
-
-" Start scrolling when 3 rows from top or bottom
-set scrolloff=3
-
-" No system sounds on errors
-set noerrorbells
-
-" Indentation uses 2 spaces
-set shiftwidth=2
-
-" Line numbers take up more space
-set numberwidth=4
-
-" Move 2 spaces when pressing tab
-set softtabstop=2
-
-" Enable terminal colors
-set termguicolors
-
-" Enables line at column 80
-set colorcolumn=80
-
-" Line numbers are relative from the current position
-set relativenumber
-
-" Enable left column for errors, warnings & information
-set signcolumn=yes
-
-" Don't show current mode in statusline
-set noshowmode
-
-" Always show tabline
-set showtabline=2
-
-" Split vertically using just "vs"
-nnoremap vs :vs<CR>
-
-" Split horizontally using just "sp"
-nnoremap sp :sp<CR>
-
-" Move one pane right using "ctrl+l"
-nnoremap <C-L> <C-W><C-L>
-
-" Move one pane left using "ctrl+h"
-nnoremap <C-H> <C-W><C-H>
-
-" Move one pane up using "ctrl+k"
-nnoremap <C-K> <C-W><C-K>
-
-" Move one pane down using "ctrl+j"
-nnoremap <C-J> <C-W><C-J>
-
-" Create a new tab using "tn"
-nnoremap tn :tabnew<CR>
-
-" Move one tab right using "tk"
-nnoremap tk :tabnext<CR>
-
-" Move one tab right using "tj"
-nnoremap tj :tabprev<CR>
-
-" Only show current tab "to"
-nnoremap to :tabo<CR>
-
-" Open explorer using "ctrl+n"
-noremap <silent> <C-n> :call ToggleNetrw()<CR>
-
-" Open
-nnoremap <C-s> :%s/
 
 " Explorer is closed by default
 let g:NetrwIsOpen=0
--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,8 +1,3 @@
-vim.api.nvim_exec(
-[[
-nnoremap <C-p> <cmd>lua require('telescope.builtin').find_files()<cr>
-nnoremap <C-f> <cmd>lua require('telescope.builtin').live_grep()<cr>
-nnoremap <C-b> <cmd>lua require('telescope.builtin').buffers()<cr>
-]],
-true
-)
+vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-B>', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
